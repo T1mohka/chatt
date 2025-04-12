a82efc4b1b6a67b7bb3d542f71915c51e48851c2
T1mohka <kurvabbb@gmail.com>
2025-04-12 19:25:00 +0300

    zrobyv chat

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const http = require('http')
 const path = require('path')
 const fs = require('fs')
 
+
 const pathToIndex = path.join(__dirname, 'static', 'index.html')
 const indexHtmlFile = fs.readFileSync(pathToIndex)
 const pathToStyle = path.join(__dirname, 'static', 'style.css')
 const styleCssFile = fs.readFileSync(pathToStyle)
 const pathToJs = path.join(__dirname, 'static', 'script.js')
 const scriptJsFile = fs.readFileSync(pathToJs)
+
 
 
 const server = http.createServer((req, res) => {
@@ -26,3 +28,13 @@ const server = http.createServer((req, res) => {
 })
 
 server.listen(3000)
+
+
+const { Server } = require('socket.io');
+const io = new Server(server);
+io.on('connection', (socket) => {
+    console.log(socket.id);
+    socket.on('new_message', (message) =>{
+        io.emit('message', message)
+    })
+})
---
+++ static/script.js
@@ -0,0 +1,20 @@
+const socket = io();
+
+const form = document.getElementById('form')
+const input = document.getElementById('input')
+const messages = document.getElementById('messages')
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    if(input.value){
+        socket.emit('new_message', input.value)
+        input.value = '';
+    }
+})
+
+socket.on('message', function(msg){
+    let item = document.createElement('li')
+    item.textContent = msg
+    messages.appendChild(item)
+    window.scrollTo(0, document.body.scrollHeight)
+})
